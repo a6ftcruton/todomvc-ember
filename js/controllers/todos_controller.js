74e9e0b59248f2fdd752052143a87e684cdb21c9
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-05-10 10:45:02 -0600

    Add controller actions to create new todos

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,22 @@
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function() {
+      // Get the title for the todo that is set in the "New Todo" input field
+      var title = this.get('newTitle')  //newTitle is the name of the value attr on the input field
+      console.log("this is " + this);
+      if (!title.trim()) { return; }
+
+      //Create the New Todo Model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      //Clear the new todo input field
+      this.set('newTitle', '');
+
+      //Save the new model
+      todo.save();
+    } 
+  }
+});
--- js/models/todo.js
+++ js/models/todo.js
@@ -1,4 +1,22 @@
-Todos.Todo = Ember.Model.extend({ 
+Todos.Todo = DS.Model.extend({ 
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+Todos.Todo.FIXTURES = [
+  {
+    id: 1,
+    title: "learn ye the embers",
+    isCompleted: false
+  },
+  {
+    id: 2,
+    title: "darlin' go and cut your hair",
+    isCompleted: false
+  },
+  {
+    id: 3,
+    title: "Wail upon thy pecs",
+    isCompleted: true
+  }
+]
